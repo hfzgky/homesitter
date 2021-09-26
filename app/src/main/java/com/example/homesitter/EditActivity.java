e2341a128c5ae194dffb138b4fa1c14e6658e62e
hxx20 <hxxxmxx@gmail.com>
2021-09-26 16:23:21 +0900

    ...

--- app/src/main/java/com/example/homesitter/EditActivity.java
+++ app/src/main/java/com/example/homesitter/EditActivity.java
@@ -3,16 +3,22 @@ package com.example.homesitter;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
+import android.content.ClipData;
 import android.content.ContentResolver;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -20,16 +26,21 @@ import android.widget.Toast;
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
-
+import java.util.ArrayList;
 
 
 public class EditActivity extends AppCompatActivity {
     private EditText mEditName;
-    private ImageButton mImagePerson;
+//    private ImageButton mImagePerson;
     private int mItem = -1; //인덱스
+    private Button mButton;
     ImageView btnSave;
     ImageButton imagePerson;
 
+    private static final String TAG = "MultiImageActivity";
+    ArrayList<Uri> uriList = new ArrayList<>();     // 이미지의 uri를 담을 ArrayList 객체
+    RecyclerView recyclerView;  // 이미지를 보여줄 리사이클러뷰
+    MultiImageAdapter adapter;  // 리사이클러뷰에 적용시킬 어댑터
 
     @SuppressLint("WrongViewCast")
     @Override
@@ -37,8 +48,9 @@ public class EditActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
 
+        mButton = findViewById(R.id.buttonGall);
         mEditName = findViewById(R.id.editTextName);
-        mImagePerson = findViewById(R.id.imagePerson);
+//        mImagePerson = findViewById(R.id.imagePerson);
 
         Intent intent = getIntent();    //intent객체를 가져옴
         if (intent != null) {    //추가
@@ -51,15 +63,19 @@ public class EditActivity extends AppCompatActivity {
         }
 
         //이미지를 클릭하면 갤러리에서 이미지를 가져옴
-        imagePerson = findViewById(R.id.imagePerson); // 이미지 객체를 얻어옴
-        imagePerson.setOnClickListener(new View.OnClickListener() {
+//        imagePerson = findViewById(R.id.imagePerson); // 이미지 객체를 얻어옴
+        mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);   //이미지를 가져오기 위해 암시적 인텐트 사용
-                intent.setType("image/*");       //모든 형태의 이미지
-                startActivityForResult(intent, 101);
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                intent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 2222);
             }
         });
+
+        recyclerView = findViewById(R.id.recyclerView);
 
         //취소버튼
         ImageButton btnBack;
@@ -95,19 +111,46 @@ public class EditActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == 101)  {
-            if(resultCode == RESULT_OK) {
-                ContentResolver contentResolver = getContentResolver();
-                try {
-                    InputStream inputStream = contentResolver.openInputStream(data.getData());    //파일 내용을 읽어옴
-                    Bitmap image = BitmapFactory.decodeStream(inputStream);
-                    imagePerson.setImageBitmap(image);   //선택한 이미지를 화면에 보여줌
-                } catch (FileNotFoundException e) {     //오류가 생길경우 토스트창으로 알려줌
-                    e.printStackTrace();
-                    Toast.makeText(getApplicationContext(), "이미지로드 오류", Toast.LENGTH_LONG).show();
+        if(requestCode == 2222) {
+            if(data == null){   // 어떤 이미지도 선택하지 않은 경우
+                Toast.makeText(getApplicationContext(), "이미지를 선택하지 않았습니다.", Toast.LENGTH_LONG).show();
+            }
+            else{   // 이미지를 하나라도 선택한 경우
+                if(data.getClipData() == null){     // 이미지를 하나만 선택한 경우
+                    Log.e("single choice: ", String.valueOf(data.getData()));
+                    Uri imageUri = data.getData();
+                    uriList.add(imageUri);
+
+                    adapter = new MultiImageAdapter(uriList, getApplicationContext());
+                    recyclerView.setAdapter(adapter);
+                    recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, true));
+                }
+                else{      // 이미지를 여러장 선택한 경우
+                    ClipData clipData = data.getClipData();
+                    Log.e("clipData", String.valueOf(clipData.getItemCount()));
+
+                    if(clipData.getItemCount() > 11){   // 선택한 이미지가 11장 이상인 경우
+                        Toast.makeText(getApplicationContext(), "사진은 11장까지 선택 가능합니다.", Toast.LENGTH_LONG).show();
+                    }
+                    else {   // 선택한 이미지가 1장 이상 10장 이하인 경우
+                        Log.e(TAG, "multiple choice");
+
+                        for (int i = 0; i < clipData.getItemCount(); i++) {
+                            Uri imageUri = clipData.getItemAt(i).getUri();  // 선택한 이미지들의 uri를 가져온다.
+                            try {
+                                uriList.add(imageUri);  //uri를 list에 담는다.
+
+                            } catch (Exception e) {
+                                Log.e(TAG, "File select error", e);
+                            }
+                        }
+
+                        adapter = new MultiImageAdapter(uriList, getApplicationContext());
+                        recyclerView.setAdapter(adapter);   // 리사이클러뷰에 어댑터 세팅
+                        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, true));     // 리사이클러뷰 수평 스크롤 적용
+                    }
                 }
             }
-
         }
     }
 }
