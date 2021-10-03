d5ae21c665c764cf216276289b216387aadbe91f
김은영 <325242@naver.com>
2021-10-03 12:23:47 +0900

    2021-10-03 FCM

--- app/src/main/java/com/example/homesitter/PushNotificationService.java
+++ app/src/main/java/com/example/homesitter/PushNotificationService.java
@@ -1,42 +1,7 @@
 package com.example.homesitter;
 
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-import android.media.RingtoneManager;
-import android.net.Uri;
-import android.os.Build;
-import android.os.Looper;
-import android.text.TextUtils;
-import android.util.Log;
-import android.widget.RemoteViews;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
-import androidx.core.app.NotificationCompat;
-import androidx.localbroadcastmanager.content.LocalBroadcastManager;
-
 import com.google.firebase.messaging.FirebaseMessagingService;
-import com.google.firebase.messaging.RemoteMessage;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class PushNotificationService extends FirebaseMessagingService {
-    private static final String TAG = "MyFirebaseMsgService";
 
-    @Override
-    public void onNewToken(String s) {
-        super.onNewToken(s);
-    }
-
-    @Override
-    public void onMessageReceived(RemoteMessage remoteMessage) {
-        Log.d(TAG, "From: " + remoteMessage.getFrom());
-        if (remoteMessage.getData().size() > 0) {
-            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
-        }
-    }
 }
